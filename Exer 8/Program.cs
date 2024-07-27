92eadddc0447195c89a7655633b1a10dcf0c67a7
TaldoGBR <165737345+TaldoGBR@users.noreply.github.com>
2024-07-27 19:59:44 -0300

    Add files via upload

---
+++ Exer 8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exer_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            for (a = 1; a <= 100; a++)
+            {
+                Console.WriteLine(a);
+            }
+;
+
+        }
+    }
+}
